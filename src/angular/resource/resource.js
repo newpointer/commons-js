77470b13a517191f7d536a6e7c3e43a242125e1d
ankostyuk <andrew.kostyuk@gmail.com>
2014-12-23 20:36:36 +0400

    Модуль resource

---
+++ src/angular/resource/resource.js
@@ -0,0 +1,12 @@
+/**
+ * @module resource
+ * @desc RequireJS/Angular module
+ * @author ankostyuk
+ */
+define(function(require) {'use strict';
+
+                  require('underscore');
+    var angular = require('angular');
+
+    return require('./np.resource');
+});
